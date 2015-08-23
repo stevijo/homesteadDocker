1e594e3cabcdbf44c3e1d0ce85c7aefc1a661885
stevijo <stevijo.mayer@googlemail.com>
2015-08-23 14:00:17 +0200

    revert

--- readme.md
+++
@@ -1,5 +0,0 @@
-# Laravel Homestead
-
-The official Laravel local development environment.
-
-Official documentation [is located here](http://laravel.com/docs/5.1/homestead).
--- scripts/clear-variables.sh
+++
@@ -1,4 +0,0 @@
-# Clear The Old Environment Variables
-
-sed -i '/# Set Homestead Environment Variable/,+1d' /home/vagrant/.profile
-sed -i '/env\[.*/,+1d' /etc/php5/fpm/php-fpm.conf
